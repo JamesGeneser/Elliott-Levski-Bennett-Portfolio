d679b911dc568c5e18440062eb4687e59902ade0
James Geneser <jamesgeneser@gmail.com>
2023-09-09 17:42:59 -0600

    react bootstrap and router dom installed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import Navigation from "../src/components/Navigation";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navigation />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
