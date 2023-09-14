96474db241d8df08031a56fe8169ee00666b1c6c
James Geneser <jamesgeneser@gmail.com>
2023-09-14 13:48:03 -0600

    modal nav item imported from portfolio

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 export const Contact = () => {
-  return <div>Contact</div>;
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <div>
+      <Button className="nav-item navElement" variant="" onClick={handleShow}>
+        Contact Me
+      </Button>
+
+      <Modal size="sm" show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title className="text-dark">Contact Me</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="contact-info">
+          <p>email: jamesgeneser@gmail.com</p>
+          <p>phone: (319)-538-3074</p>
+        </Modal.Body>
+        <Modal.Footer></Modal.Footer>
+      </Modal>
+    </div>
+  );
 };
