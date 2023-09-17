79b2c055bde9dd047b2c510be7103beb17711f7b
James Geneser <jamesgeneser@gmail.com>
2023-09-17 15:22:53 -0600

    profile img and formatting changed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,27 +7,47 @@ import Col from "react-bootstrap/Col";
 export default function Home() {
   return (
     <div>
-      <Container fluid className="introduction">
-        <h1>Elliott Levski Bennett</h1>
-        <h3>Journeyman Filmmaker</h3>
+      <Container
+        fluid
+        className="introduction-container mt-4 mb-4 d-flex flex-column justify-content-start"
+      >
+        <h1 className="title ms-3 ps-4">Elliott Levski Bennett</h1>
+        <h3 className="sub-title ms-5 ps-5">Journeyman Filmmaker</h3>
       </Container>
-
-      <Card className="contact-information m-4 p-1">
-        <Card.Title className="m-2">Contact Me</Card.Title>
-        <Card.Body>
-          <Row>
-            <Col xs={4}>E-mail:</Col>
-            <Col xs={8}>elliott-bennett@email.com</Col>
-          </Row>
-          <Row>
-            <Col xs={4}>Phone:</Col>
-            <Col xs={8}>(xxx)-xxx-xxxx</Col>
-          </Row>
-          <Row className="travel-preference">
-            Pheonix-based. Open to travel in America and Canada.
-          </Row>
-        </Card.Body>
-      </Card>
+      <Container fluid className="contact-container d-flex flex-row-reverse">
+        <Card className="contact-information-card m-4 me-5 p-3 pe-5 ">
+          <Card.Title className="ms-2 mb-0">Contact Me</Card.Title>
+          <hr />
+          <Card.Body className="mt-0 pt-0">
+            <Row>
+              <Col xs={4}>E-mail:</Col>
+              <Col xs={8}>elliott-bennett@email.com</Col>
+            </Row>
+            <Row>
+              <Col xs={4}>Phone:</Col>
+              <Col xs={8}>(xxx)-xxx-xxxx</Col>
+            </Row>
+            <Row className="travel-information">
+              <Col> Pheonix-based. Open to travel in America and Canada.</Col>
+            </Row>
+          </Card.Body>
+        </Card>
+      </Container>
+      <Container className="about-me-container">
+        <Card className="about-me-card">
+          <Card.Img
+            className="profile-home"
+            src={require("../media/elliott-icecream.jpeg")}
+          ></Card.Img>
+          <Card.Body>
+            <h5>Filmmaker. Performer. Storyteller.</h5>
+            <p>
+              Passionately curious about the world, its history, and the people
+              in it.
+            </p>
+          </Card.Body>
+        </Card>
+      </Container>
     </div>
   );
 }
