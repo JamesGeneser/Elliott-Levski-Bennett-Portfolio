5c90202fda80221c7a961a070d7f42076281b2f8
James Geneser <jamesgeneser@gmail.com>
2023-09-28 14:37:47 -0600

    selective rendering of the navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,19 +3,47 @@ import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import { Contact } from "./Contact";
 import Brand from "./Brand";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function Navigation() {
-  console.log(window.location.href);
+  const location = useLocation();
+  //   console.log(location.pathname);
+
+  //   const [home, setHome] = useState();
+
+  //   useEffect(() => {
+  //     const isHome = (location.pathname = "/") ? true : false;
+  //     setHome((isHome = true ? (home = true) : (home = false)));
+  //   });
+
+  //   if (location.pathname == "/") {
+  //     setHome(home === true);
+  //   } else {
+  //     setHome(home === false);
+  //   }
+  //   let isHome = (location.pathname = "/" ? true : false);
+  //   console.log(isHome);
+
+  //   function homePage() {
+  //     const home = (location = "/" ? true : false);
+  //   }
+
+  //   setHome();
+  //   {
+  //     home = location = "/" ? true : false;
+  //   }
   return (
     <>
-      <Navbar expand="md" className=" ">
+      <Navbar expand="lg" className=" ">
         {" "}
         <Container fluid className="">
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav" className="">
             <Nav className="nav me-auto d-flex justify-content-around">
               {" "}
-              <Nav.Link href="/">Home</Nav.Link>
+              {location.pathname != "/" && <Nav.Link href="/">Home</Nav.Link>}
               <Nav.Link href="/video">Video</Nav.Link>
               <Nav.Link href="/live-performance">Live Performance</Nav.Link>
               <Nav.Link href="/art-design">Art/Design</Nav.Link>
@@ -24,7 +52,7 @@ export default function Navigation() {
             </Nav>
           </Navbar.Collapse>
 
-          <Brand />
+          {location.pathname != "/" && <Brand />}
         </Container>
       </Navbar>
     </>
